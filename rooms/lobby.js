3da84cafc3690ca44acb8c4eb1b0f5e0187570d3
Alexey <redspirit@live.ru>
2024-01-20 19:23:14 +0300

    set brightness

--- rooms/lobby.js
+++ rooms/lobby.js
@@ -1,9 +1,18 @@
 
+const moment = require('moment');
 const Device = require('../modules/Device');
 
 const lobbyMotion = new Device('lobby_motion');
 const ledDriver = new Device('lobby_led_driver');
 
 lobbyMotion.on(async (data) => {
-    ledDriver.set({state: data.occupancy ? 'ON' : 'OFF'})
+    let hour = moment().hours();
+    let isNight = hour > 22 || hour < 7;
+    let brightness = isNight ? 50 : 254;
+
+    let state = data.occupancy ? 'ON' : 'OFF';
+    ledDriver.set({
+        state,
+        brightness
+    });
 });
