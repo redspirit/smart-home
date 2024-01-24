67769014cdfd928ae3d617acb6904bf440d28250
Alexey <redspirit@live.ru>
2024-01-24 21:09:09 +0300

    remove log

--- rooms/lobby.js
+++ rooms/lobby.js
@@ -10,8 +10,6 @@ lobbyMotion.on(async (data) => {
     let isNight = hour > 22 || hour < 7;
     let brightness = isNight ? 40 : 254;
 
-    console.log({hour, isNight, brightness});
-
     let state = data.occupancy ? 'ON' : 'OFF';
     ledDriver.set({
         state,
