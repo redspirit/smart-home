be949eca610463e1b9b92ad37a2bdeaa94702ea3
Alexey <redspirit@live.ru>
2024-06-17 20:07:24 +0300

    Update lobby.js

--- rooms/lobby.js
+++ rooms/lobby.js
@@ -7,7 +7,7 @@ const ledDriver = new Device('lobby_led_driver');
 
 lobbyMotion.onMessage((data) => {
     let hour = moment().hours();
-    let isNight = hour > 21 || hour < 7;
+    let isNight = hour > 19 || hour < 7;
     let brightness = isNight ? 30 : 254;
 
     let state = data.occupancy ? 'ON' : 'OFF';
